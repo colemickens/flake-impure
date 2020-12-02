e734639eb1ab0f983bd683d635d75e17292ae7bc
Cole Mickens <cole.mickens@gmail.com>
2020-12-01 21:18:07 -0800

    multi-arch

--- flake.nix
+++ flake.nix
@@ -8,21 +8,32 @@
 
   outputs = inputs:
   let
-    pkgs = import inputs.nixpkgs {system="x86_64-linux";};
-    mkPkg = num:
-      pkgs.runCommandNoCC "foo${toString num}.txt" {} ''
+    nameValuePair = name: value: { inherit name value; };
+    genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = genAttrs supportedSystems;
+
+    pkgsFor = pkgs: sys: import pkgs {
+      system = sys;
+      config = { allowUnfree = true; };
+    };
+    pkgs_ = genAttrs (builtins.attrNames inputs) (inp: genAttrs supportedSystems (sys: pkgsFor inputs."${inp}" sys));
+
+    mkPkg = sys: num:
+      pkgs_.nixpkgs."${sys}".runCommandNoCC "foo${toString num}.txt" {} ''
         sleep $(( 1 * ${toString num} ))
         echo ${toString builtins.currentTime} >> $out
       '';
   in
   {
-    packages.x86_64-linux = {
-      test1 = mkPkg 1;
-      #test2 = mkPkg 2;
-      #test3 = mkPkg 3;
-    };
-    defaultPackage.x86_64-linux =
-      pkgs.linkFarmFromDrvs "bundle" (builtins.attrValues inputs.self.packages.x86_64-linux);
+    packages = forAllSystems (sys:
+      { test1 = mkPkg sys 1;}
+    );
+
+    defaultPackage = forAllSystems (sys:
+      pkgs_.nixpkgs."${sys}".linkFarmFromDrvs "bundle"
+        (builtins.attrValues inputs.self.packages."${sys}")
+    );
   };
 }
 
